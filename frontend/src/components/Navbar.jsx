ff7ea2035e591206dc4540175e31e5136fb3cf1c
Dev~ <devusays@gmail.com>
2025-05-07 17:10:43 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,63 +1,46 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAuthStore } from "../store/useAuthStore";
 import { LogOut, MessageSquare, Settings, User } from "lucide-react";
 
 const Navbar = () => {
   const { logout, authUser } = useAuthStore();
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  const handleToggleRoute = (route: string) => {
-    if (location.pathname === route) {
-      navigate("/");
-    } else {
-      navigate(route);
-    }
-  };
 
   return (
     <header
       className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40 
-      backdrop-blur-lg bg-base-100/80"
+    backdrop-blur-lg bg-base-100/80"
     >
       <div className="container mx-auto px-4 h-16">
         <div className="flex items-center justify-between h-full">
           <div className="flex items-center gap-8">
-            <button
-              onClick={() => navigate("/")}
-              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
-            >
+            <Link to="/" className="flex items-center gap-2.5 hover:opacity-80 transition-all">
               <div className="size-9 rounded-lg bg-primary/10 flex items-center justify-center">
                 <MessageSquare className="w-5 h-5 text-primary" />
               </div>
               <h1 className="text-lg font-bold">Chatty</h1>
-            </button>
+            </Link>
           </div>
 
           <div className="flex items-center gap-2">
-            <button
-              onClick={() => handleToggleRoute("/settings")}
-              className="btn btn-sm gap-2 transition-colors"
+            <Link
+              to={"/settings"}
+              className={`
+              btn btn-sm gap-2 transition-colors
+              
+              `}
             >
               <Settings className="w-4 h-4" />
               <span className="hidden sm:inline">Settings</span>
-            </button>
+            </Link>
 
             {authUser && (
               <>
-                <button
-                  onClick={() => handleToggleRoute("/profile")}
-                  className="btn btn-sm gap-2"
-                >
+                <Link to={"/profile"} className={`btn btn-sm gap-2`}>
                   <User className="size-5" />
                   <span className="hidden sm:inline">Profile</span>
-                </button>
+                </Link>
 
-                <button
-                  onClick={logout}
-                  className="btn btn-sm gap-2"
-                  aria-label="Logout"
-                >
+                <button className="flex gap-2 items-center" onClick={logout}>
                   <LogOut className="size-5" />
                   <span className="hidden sm:inline">Logout</span>
                 </button>
@@ -69,5 +52,4 @@ const Navbar = () => {
     </header>
   );
 };
-
 export default Navbar;
