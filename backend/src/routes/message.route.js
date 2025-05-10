512d99eab68e1d40c469d645b403ec998381c395
Dev~ <devusays@gmail.com>
2025-05-10 19:52:04 +0530

    Update message.route.js

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,12 +1,20 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
+import {
+  getMessages,
+  getUsersForSidebar,
+  sendMessage,
+  deleteMessage, // ⬅️ Import it
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
-
 router.post("/send/:id", protectRoute, sendMessage);
 
+// ⬇️ New delete route
+router.delete("/:id", protectRoute, deleteMessage);
+
 export default router;
+
